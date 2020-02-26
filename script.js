e4e6011ea225e8d5078eda89544d448adda24080
Ben Cross <ben_cross@me.com>
2020-02-26 19:23:49 +0000

    added autoplay on click

---
+++ script.js
@@ -0,0 +1,27 @@
+var overlayShow = true
+
+
+
+document.getElementById("videoOverlayDesktop").onclick = function(e) {
+  overlayShow = false
+  e.target.style.visibility = 'hidden';
+  document.getElementById("iframeID").src = iframeSRC;
+
+}
+
+
+document.getElementById("videoOverlayMobile").onclick = function(e) {
+  overlayShow = false
+  e.target.style.visibility = 'hidden';
+  document.getElementById("iframeID").src = iframeSRC;
+
+}
+
+
+
+
+
+var iframeSRC = document.getElementById("iframeID").src+"?autoplay=1";
+document.getElementById("iframeID").src = iframeSRC;
+
+console.log(iframeSRC);
